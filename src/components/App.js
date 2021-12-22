5029bb91aef2b98305d6d06fe1da4186ed807764
Rizwan <rizwankhalid171@gmail.com>
2021-12-22 20:39:16 +0000

    Removed MUI related import

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import List from './List/main/List.js';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
 
 function App() {
   const theme = createTheme();
 
   return (
     <div>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <List />
-      </ThemeProvider>
+      <List />
     </div>
   );
 }
