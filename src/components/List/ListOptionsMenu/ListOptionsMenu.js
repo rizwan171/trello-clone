c99034e4addfd98d960e9d1dba7768b064ad5653
rizwan171 <43914992+rizwan171@users.noreply.github.com>
2022-02-16 19:13:35 +0000

    Fixed closing the list options menu when clicking outside the menu

--- src/components/List/ListOptionsMenu/ListOptionsMenu.js
+++ src/components/List/ListOptionsMenu/ListOptionsMenu.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
@@ -8,8 +8,21 @@ import { removeList } from "../../../features/listsSlice";
 
 const ListOptionsMenu = ({ list }) => {
   const dispatch = useDispatch();
+  const ref = useRef();
+  const currentSelectedList = useSelector((state) => state.currentSelectedList.value);
   const positionData = useSelector((state) => state.listOptionsMenuPosition.value);
   const [styles, setStyles] = useState({});
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      if (currentSelectedList && ref.current && !ref.current.contains(e.target)) {
+        dispatch(clearSelectedList());
+      }
+    };
+
+    document.addEventListener("mousedown", checkIfClickedOutside);
+    return () => document.removeEventListener("mousedown", checkIfClickedOutside);  
+  }, [currentSelectedList]);
 
   useEffect(() => {
     if (positionData) {
@@ -28,7 +41,7 @@ const ListOptionsMenu = ({ list }) => {
 
   return (
     // TODO add a transition for height when this is shown
-    <div style={styles} className="flex flex-col rounded-sm shadow-xl w-24 text-center fixed bg-trello-gray-400">
+    <div ref={ref} style={styles} className="flex flex-col rounded-sm shadow-xl w-24 text-center fixed bg-trello-gray-400">
       <button className="w-full p-2 cursor-pointer hover:bg-black hover:bg-opacity-20 rounded-md">Copy</button>
       <button className="w-full p-2 cursor-pointer hover:bg-black hover:bg-opacity-20 rounded-md">Move</button>
       <button className="w-full p-2 cursor-pointer hover:bg-black hover:bg-opacity-20 rounded-md" onClick={handleDelete}>
