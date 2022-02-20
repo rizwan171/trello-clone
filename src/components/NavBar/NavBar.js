47a379725e1184954e0e7153d29650e83f402bf8
rizwan171 <43914992+rizwan171@users.noreply.github.com>
2022-02-20 21:34:47 +0000

    Updated board title to use the new slice

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <div className="w-full px-6 pt-3">
       <div className="flex w-full h-14 m-1">
-        <BoardTitle boardTitle={"Board 1Board 1Board 1Board 1Board 1Board 1"} />
+        <BoardTitle />
         <BoardOptions />
       </div>
     </div>
