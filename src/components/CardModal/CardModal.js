3e9ec8277ed9fe9be89f11ba705d295812f83a25
rizwan171 <43914992+rizwan171@users.noreply.github.com>
2022-05-11 19:51:57 +0100

    WIP Designed the Create Tag form UI

--- src/components/CardModal/CardModal.js
+++ src/components/CardModal/CardModal.js
@@ -6,7 +6,7 @@ import { clearSelectedCard, setCurrentSelectedCard } from "../../features/curren
 import { useSelector } from "react-redux";
 import { AiOutlineTag, AiOutlineDelete } from "react-icons/ai";
 import { HiOutlineArrowRight } from "react-icons/hi";
-import { MdOutlineContentCopy, MdInbox, MdClose, MdOutlineEdit, MdCheck } from "react-icons/md";
+import { MdOutlineContentCopy, MdInbox, MdClose, MdOutlineEdit, MdCheck, MdArrowBack, MdClear } from "react-icons/md";
 import { FiAlignLeft } from "react-icons/fi";
 import CardModalTag from "./CardModalTag/CardModalTag";
 import AddTag from "./AddTag/AddTag";
@@ -26,6 +26,8 @@ const CardModal = ({ card }) => {
   const [editableContent, setEditableContent] = useState(card.content);
   const [rows, setRows] = useState(1);
   const [tagsMenuStyle, setTagsMenuStyle] = useState({});
+  // TODO set to false after
+  const [createTagOpen, setCreateTagOpen] = useState(true);
 
   useEffect(() => {
     if (ref && ref.current) {
@@ -104,6 +106,8 @@ const CardModal = ({ card }) => {
     setRows(Math.ceil(e.target.scrollHeight / e.target.clientHeight));
   };
 
+  const showCreateTagForm = () => {};
+
   return (
     <div
       id="cardModal"
@@ -180,29 +184,74 @@ const CardModal = ({ card }) => {
               <p>Tags</p>
             </button>
             {/* TODO min-h-60 should be changed later to something more appropriate */}
-            <div className="fixed w-72 min-h-60 text-gray-700 bg-white rounded-ibsm shadow-2xl p-4 ">
-              <div className="relative text-center mb-2">
-                <span className="text-sm block relative z-10">Tags</span>
-                <MdClose size={20} className="absolute right-0 top-0 z-20" />
-              </div>
-              <hr />
-              <div className="flex flex-col mt-2 text-sm">
-                <input
-                  type="text"
-                  className="w-full h-9 py-1 px-2 border-1 mb-2 border-gray-200 rounded-sm focus:outline-none focus:ring-2 focus:ring-trello-blue-100"
-                  placeholder="Search tags..."
-                />
-                <span className="text-gray-600 my-2 font-semibold">Tags</span>
-                <div className="flex flex-col w-full gap-1 font-bold">
-                  {tags.map((tag) => (
-                    <TagOption key={tag.id} name={tag.name} />
-                  ))}
+            {!createTagOpen ? (
+              <div className="fixed w-72 min-h-60 text-gray-700 bg-white rounded-ibsm shadow-2xl p-4 ">
+                <div className="relative text-center mb-2">
+                  <span className="text-sm block relative z-10">Tags</span>
+                  <MdClose size={20} className="absolute right-0 top-0 z-20" />
                 </div>
-                <button className="py-1.5 px-2 mt-3 bg-trello-gray-card-modal-buttons hover:bg-trello-gray-card-modal-buttons-hover text-trello-blue-card-modal-button-text items-center text-sm shadow-sm rounded-sm">
-                  Create a new tag
-                </button>
+                <hr />
+                <div className="flex flex-col mt-2 text-sm">
+                  <input
+                    type="text"
+                    className="w-full h-9 py-1 px-2 border-1 mb-2 border-gray-200 rounded-sm focus:outline-none focus:ring-2 focus:ring-trello-blue-100"
+                    placeholder="Search tags..."
+                  />
+                  <span className="text-gray-600 my-2 font-semibold">Tags</span>
+                  <div className="flex flex-col w-full gap-1 font-bold">
+                    {tags.map((tag) => (
+                      <TagOption key={tag.id} name={tag.name} />
+                    ))}
+                  </div>
+                  <button
+                    onClick={showCreateTagForm}
+                    className="py-1.5 px-2 mt-3 bg-trello-gray-card-modal-buttons hover:bg-trello-gray-card-modal-buttons-hover text-trello-blue-card-modal-button-text items-center text-sm shadow-sm rounded-sm"
+                  >
+                    Create a new tag
+                  </button>
+                </div>
               </div>
-            </div>
+            ) : (
+              <div className="fixed w-72 min-h-40 text-gray-700 bg-white rounded-ibsm shadow-2xl p-4 ">
+                <div className="relative text-center mb-2">
+                  <span className="text-sm block relative z-10">Tags</span>
+                  <MdArrowBack size={20} className="absolute left-0 top-0 z-20" />
+                  <MdClose size={20} className="absolute right-0 top-0 z-20" />
+                </div>
+                <hr />
+                <div className="flex flex-col mt-2 text-sm">
+                  <span className="text-gray-600 my-2 font-semibold">Name</span>
+                  <input
+                    type="text"
+                    className="w-full h-9 py-1 px-2 border-1 mb-2 border-gray-200 rounded-sm focus:outline-none focus:ring-2 focus:ring-trello-blue-100"
+                  />
+                  <span className="text-gray-600 my-2 font-semibold">Select a colour</span>
+                  <div className="grid grid-cols-5 gap-2">
+                    <div className="h-11 w-12 rounded-md bg-black "></div>
+                    <div className="h-11 w-12 rounded-md bg-black "></div>
+                    <div className="h-11 w-12 rounded-md bg-black "></div>
+                    <div className="h-11 w-12 rounded-md bg-black "></div>
+                    <div className="h-11 w-12 rounded-md bg-black "></div>
+                    <div className="h-11 w-12 rounded-md bg-black "></div>
+                    <div className="h-11 w-12 rounded-md bg-black "></div>
+                    <div className="h-11 w-12 rounded-md bg-black "></div>
+                    <div className="h-11 w-12 rounded-md bg-black "></div>
+                    <div className="h-11 w-12 rounded-md bg-black "></div>
+                    <div className="h-11 w-12 rounded-md bg-trello-gray-card-modal-buttons flex justify-center items-center">
+                      <MdClear size={20}/>
+                    </div>
+                    <div className="col-span-3 flex flex-col">
+                      <span className="text-gray-600">No Colour</span>
+                      <span className="text-gray-400">This wont show up on the front of cards</span>
+                    </div>
+                  </div>
+                  <button className="p-2 mt-3 bg-trello-blue-100 hover:bg-trello-blue-200 text-white font-semibold items-center text-sm shadow-sm rounded-sm">
+                    Create
+                  </button>
+                </div>
+              </div>
+            )}
+
             <h4 className="text-gray-800 text-sm">Actions</h4>
             <button className="flex gap-2 py-1 px-2 mb-2 bg-trello-gray-card-modal-buttons hover:bg-trello-gray-card-modal-buttons-hover text-trello-blue-card-modal-button-text items-center text-base shadow-sm rounded-sm">
               <MdOutlineContentCopy />
