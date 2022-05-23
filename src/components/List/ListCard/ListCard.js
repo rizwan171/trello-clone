e906224d0e4032ffdd4e9156133c57c5fc8420ad
rizwan171 <43914992+rizwan171@users.noreply.github.com>
2022-05-23 18:13:02 +0100

    Updated ListCard to show tags selected on the card

--- src/components/List/ListCard/ListCard.js
+++ src/components/List/ListCard/ListCard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Draggable } from "react-beautiful-dnd";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setCurrentSelectedCard } from "../../../features/currentSelectedCardSlice";
+import ListCardTag from "./ListCardTag/ListCardTag";
 
 const ListCard = ({ card, index }) => {
   const dispatch = useDispatch();
+  const tagsToShow = useSelector((state) => state.tags.value).filter((tag) => card.tags.includes(tag.id));
 
   const openModal = () => {
     dispatch(setCurrentSelectedCard(card));
@@ -17,21 +19,13 @@ const ListCard = ({ card, index }) => {
         <div ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps} onClick={openModal}>
           <div className="flex flex-col shadow bg-white hover:bg-trello-gray-400 cursor-pointer rounded-md p-2 my-1.5 break-words">
             <p>{card.content}</p>
-            <div className="pt-2 text-xs flex flex-wrap gap-1">
-              <div className="bg-blue-500 rounded-md p-1">Tasdfasdfasdf 1</div>
-              <div className="bg-yellow-500 rounded-md p-1">Tag 1</div>
-              <div className="bg-green-200 rounded-md p-1">Tag 1</div>
-              <div className="bg-indigo-700 rounded-md p-1">Tag 1</div>
-              <div className="bg-blue-500 rounded-md p-1">Tag 1</div>
-              <div className="bg-blue-500 rounded-md p-1">Tag 1</div>
-              <div className="bg-blue-500 rounded-md p-1">Tag 1</div>
-              <div className="bg-blue-500 rounded-md p-1">Tag 1</div>
-              <div className="bg-blue-500 rounded-md p-1">Tag 1</div>
-              <div className="bg-blue-500 rounded-md p-1">Tag 1</div>
-              <div className="bg-blue-500 rounded-md p-1">Tag 1</div>
-              <div className="bg-blue-500 rounded-md p-1">Tag 1</div>
-              <div className="bg-blue-500 rounded-md p-1">Tag 1</div>
-            </div>
+            {card.tags.length > 0 && (
+              <div className="pt-2 text-xs flex flex-wrap gap-1">
+                {tagsToShow.map((tag) => (
+                  <ListCardTag key={tag.id} tag={tag} />
+                ))}
+              </div>
+            )}
           </div>
         </div>
       )}
