a06e13ee584da7d38c52c00523806c1d1b290e73
rizwan171 <43914992+rizwan171@users.noreply.github.com>
2022-05-23 18:26:21 +0100

    Added margin to move tags menus under tag button

--- src/components/CardModal/TagsMenu/TagsMenu.js
+++ src/components/CardModal/TagsMenu/TagsMenu.js
@@ -37,7 +37,7 @@ const TagsMenu = ({ card, closeTags, showCreateTagForm }) => {
 
   return (
     // TODO need to determine max height and then scroll tags
-    <div className="fixed w-72 text-gray-700 bg-white rounded-ibsm shadow-2xl p-4 ">
+    <div className="fixed mt-16 w-72 text-gray-700 bg-white rounded-ibsm shadow-2xl p-4">
       <div className="relative text-center mb-2">
         <span className="text-sm block relative z-10">Tags</span>
         <MdClose onClick={closeTags} size={20} className="absolute right-0 top-0 z-20 cursor-pointer" />
