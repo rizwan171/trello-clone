5d6b3f89a314ef55332ac432e520f9d5944634c4
nk711 <nkoneswaran123@gmail.com>
2022-06-08 00:40:41 +0100

    Added attachment field when creating a card

--- src/components/List/AddCard/AddCardForm/AddCardForm.js
+++ src/components/List/AddCard/AddCardForm/AddCardForm.js
@@ -30,7 +30,7 @@ const AddCardForm = ({ setOpen, open, listId }) => {
 
   const handleAddCard = () => {
     if (text.trim().length !== 0) {
-      dispatch(addCard({ listId, title: text, tags: [], description: "" }));
+      dispatch(addCard({ listId, title: text, tags: [], description: "", attachments: []}));
     }
     handleClose();
   };
