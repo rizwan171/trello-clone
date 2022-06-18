3ee27cc91e1f9bb013fd6d85674b72dc2d3c10dc
rizwan171 <43914992+rizwan171@users.noreply.github.com>
2022-06-18 23:14:48 +0100

    Changed app render to use createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
 import App from "./components/App";
 import store from "./app/store";
 import "./css/index.css";
 
-ReactDOM.render(
+const rootDiv = document.getElementById("root");
+if (!rootDiv) {
+  throw new Error("Root container missing in index.html");
+}
+
+const root = createRoot(rootDiv);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
