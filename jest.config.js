11a3545aa817c3169c6ce77b01809e53eb5a88ed
rizwan171 <43914992+rizwan171@users.noreply.github.com>
2022-07-09 12:54:06 +0100

    Added libraries and config for using jsdom and eslint in jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   verbose: true,
+  setupFilesAfterEnv: ["<rootDir>/src/__test__/utils/setupTests.js"],
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
 };
