f34b6ae17c5d1fba6c47a18373dc0066b6550b48
rizwan171 <43914992+rizwan171@users.noreply.github.com>
2022-07-13 19:55:33 +0100

    Updated component styling and removed unused file

--- src/components/BoardOptionsMenu/ExportModal/ExportModal.tsx
+++ src/components/BoardOptionsMenu/ExportModal/ExportModal.tsx
@@ -16,7 +16,7 @@ const ExportModal = ({ closeExportModal, handleExportList }: ExportModalProps) =
       className="z-50 min-h-full min-w-full flex fixed top-0 left-0 justify-center items-center bg-black bg-opacity-30"
     >
       <div className="relative px-4 w-full max-w-sm h-full md:h-auto mb-36">
-        <div className="relative bg-trello-gray-100 rounded-lg shadow dark:bg-gray-700">
+        <div className="relative bg-trello-gray-100 rounded-lg shadow">
           <div className="flex p-6">
             <div>
               <select
@@ -40,14 +40,14 @@ const ExportModal = ({ closeExportModal, handleExportList }: ExportModalProps) =
           <div className="flex items-center px-6 pb-6">
             <button
               type="button"
-              className="text-white bg-trello-green-100 hover:bg-trello-green-200 text-base rounded-lg px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-trello-green-100 hover:bg-trello-green-200 text-base rounded-lg px-5 py-2.5 text-center"
               onClick={() => handleExportList(selectedListId)}
             >
               Export
             </button>
             <button
               type="button"
-              className="text-gray-600 bg-gray-300 text-base rounded-lg ml-2 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-gray-600 bg-gray-300 text-base rounded-lg ml-2 px-5 py-2.5 text-center hover:bg-gray-400"
               onClick={() => closeExportModal()}
             >
               Cancel
