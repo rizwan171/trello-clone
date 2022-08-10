3f77cbd2f75ddcd7a5f6a55a9718e2b395e4b9e8
rizwan171 <43914992+rizwan171@users.noreply.github.com>
2022-08-10 23:23:29 +0100

    Added tests for TagsMenu

--- src/components/CardModal/CardModalActions/TagsMenu/TagOption/TagOption.tsx
+++ src/components/CardModal/CardModalActions/TagsMenu/TagOption/TagOption.tsx
@@ -22,8 +22,8 @@ const TagOption = ({ tag, isSelected, editTag, tagClicked }: TagOptionProps) => 
   return (
     <div className="flex h-8">
       <div data-testid="tag-option-inner-container" ref={tagRef} className="items-center flex text-white w-full rounded-ibsm cursor-pointer" onClick={handleClick}>
-        <span className="ml-2">{tag.name}</span>
-        {isSelected && <MdCheck size={20} className="ml-auto mr-2" />}
+        <span data-testid="tag-option-name" className="ml-2">{tag.name}</span>
+        {isSelected && <MdCheck data-testid="tag-selected-icon" size={20} className="ml-auto mr-2" />}
       </div>
       <MdOutlineEdit data-testid="edit-icon" className="self-center ml-2 cursor-pointer" size={20} onClick={handleEdit} />
     </div>
