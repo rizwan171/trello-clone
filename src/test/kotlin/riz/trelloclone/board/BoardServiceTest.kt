2c6e9dfbe5232bde855ae7c1bd8ad81e1a562143
Rizwan <rizwankhalid171@gmail.com>
2023-01-11 19:50:56 +0000

    Updated service tests

--- src/test/kotlin/riz/trelloclone/board/BoardServiceTest.kt
+++ src/test/kotlin/riz/trelloclone/board/BoardServiceTest.kt
@@ -32,4 +32,23 @@ class BoardServiceTest {
       .usingRecursiveComparison()
       .isEqualTo(boardJson)
   }
+
+  @Test
+  fun updateBoard() {
+    val boardId = UUID.randomUUID()
+    val board = Board(boardId, "title")
+    val updatedBoard = Board(boardId, "Updated Title")
+    val updatedBoardJson = BoardJson(boardId, "Updated Title")
+    every { boardRepository.save(any()) } returns updatedBoard
+
+    val savedJsonBoard = boardService.updateBoard(board, updatedBoardJson)
+
+    verify { boardRepository.save(capture(boardCaptureSlot)) }
+    assertThat(boardCaptureSlot.captured)
+      .usingRecursiveComparison()
+      .isEqualTo(updatedBoard)
+    assertThat(savedJsonBoard)
+      .usingRecursiveComparison()
+      .isEqualTo(updatedBoardJson)
+  }
 }
